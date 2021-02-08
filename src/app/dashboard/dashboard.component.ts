8447aef18cae9ec01c5d4c5674505078392c8592
shakirkagzi <shakirkagzi@gmail.com>
2021-02-07 23:54:18 -0500

    HW3 submitted.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  imageUrl  = 'https://miro.medium.com/max/560/1*fIY4dIvymVxVHKHV5NNRNw.png';
+  public name = 'Shakir';
+  public title = 'Home page';
+  isAuthenticate = true;
+  isActive = true;
+  public duration = 10;
+  public text = 'I am from type script class';
+  public userid = 'Username';
+  public password = 'Password';
+  public email = 'email';
+  actionName = 'Aria Enhancement';
+  destinationUrl = 'https://google.com';
+  alternativeUrl = 'https:yahoo.com';
+  public username: any;
+  public isSpecial = false;
+  public clickCounter = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log('Current value of username-- ', this.username);
   }
-
+  onClick(): void {
+    this.clickCounter ++;
+    console.log('User just clicked');
+  }
+  toggleColor(): void {
+    console.log('Inside the toggleColor');
+  }
+  onKeyPressed(event: any): void {
+      console.log('User pressed the Key!', event.key);
+  }
 }
